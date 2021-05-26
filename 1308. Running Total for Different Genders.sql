4321654af7983cf2a1f83eaf8882dfd15e8bee74
Prajakta Pingale <59905994+prajaktapingale@users.noreply.github.com>
2021-05-26 13:57:43 -0700

    Create 1308. Running Total for Different Genders.sql

---
+++ 1308. Running Total for Different Genders.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select gender, day, 
+sum(score_points) over (partition by gender order by day) as total
+from scores
+group by gender, day
+order by gender, day
