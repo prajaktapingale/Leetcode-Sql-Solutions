cdcd43dfb3aa45e2d22c73bee155d3d6959dd09c
Prajakta Pingale <59905994+prajaktapingale@users.noreply.github.com>
2021-08-14 11:45:31 -0400

    Create 1322. Ads Performance.sql

---
+++ 1322. Ads Performance.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+
+select ad_id, coalesce(round(avg(case 
+when action='Clicked' then 1
+when action='Viewed' then 0 end)*100,2),0) as ctr
+from Ads
+group by ad_id
+order by ctr desc, ad_id
